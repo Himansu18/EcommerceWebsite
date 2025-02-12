5880cf6fce41bd209f9db830b73a1c07d6b67a83
Himansu18 <himansub903@gmail.com>
2025-02-12 20:06:03 +0530

    Product Model View file setup

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,8 @@ const app = express()
 const mongoose=require("mongoose");
 const dotenv=require("dotenv")
 const main=require("./config/db")
-
+const path=require('path');
+const engine=require("ejs-mate")
 //config dot env
 dotenv.config();
 
@@ -16,6 +17,17 @@ const adminModel=require("./models/adminModel")
 //db connection
 main();
 
+//All Middlewares
+app.set('view engine','ejs');
+app.set('views', 'D:/ECOMMERCE/Backend/views');
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+app.engine('ejs', engine);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -23,7 +35,8 @@ app.get('/', (req, res) => {
 app.get("/api/products",async (req,res)=>{
   try{
     let allData=await productModel.find({});
-    res.send(allData);
+    //console.log(allData)
+    res.render("ProductPages/HomePage.ejs",{allData});
     }catch(e){
     res.send(e.message)
   }
@@ -31,8 +44,8 @@ app.get("/api/products",async (req,res)=>{
 app.get("/api/products/:id",async (req,res)=>{
   try{
     let {id}=req.params;
-    let data=await productModel.findById({_id:id});
-    res.send(data);
+    let idData=await productModel.findById({_id:id});
+    res.render("ProductPages/IdPage.ejs",{idData});
     }catch(e){
     res.send(e.message)
   }
@@ -40,10 +53,10 @@ app.get("/api/products/:id",async (req,res)=>{
 app.get("/api/products/category/:category",async (req,res)=>{
   try{
     let {category}=req.params;
-    let categorydata=await productModel.find({category:category});
-    res.send(categorydata);
+    let categoryData=await productModel.find({category:category});
+    res.render("ProductPages/CategoryPage.ejs",{categoryData});
     }catch(e){
-    res.send(e.message)
+    res.render(e.message)
   }
 })
 
